086e0d53f00e9d646defcaa6fe1ec21e934ee8da
Nataliya S <solnatae@gmail.com>
2020-08-14 10:05:27 +0300

    moved methods to BasePage

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,15 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators():
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+class MainPageLocators():
+    pass
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
