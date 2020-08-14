d61d953316e43ec9a24bfe5a442a02df910b5176
Nataliya S <solnatae@gmail.com>
2020-08-14 09:31:58 +0300

    added negative tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -15,3 +15,11 @@ class ProductPage(BasePage):
         actual_product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         basket_product_price = self.browser.find_element(*ProductPageLocators.CONFIRM_MESSAGE_PRODUCT_PRICE).text
         assert actual_product_price == basket_product_price, "Product price is not correct"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.CONFIRM_MESSAGE_PRODUCT_NAME), \
+            "Success message is presented, but should not be"
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.CONFIRM_MESSAGE_PRODUCT_NAME), \
+            "Success message does not disappear as it should"
