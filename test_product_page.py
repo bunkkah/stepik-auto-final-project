c95f3c718b449bb65525628a23fe4eaa201dae08
Nataliya S <solnatae@gmail.com>
2020-08-27 20:12:49 +0300

    cleared up links in test

--- test_product_page.py
+++ test_product_page.py
@@ -36,20 +36,9 @@ class TestUserAddToBasketFromProductPage:
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('link',
-                         ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                          pytest.param(
-                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                              marks=pytest.mark.xfail),
-                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
 def test_guest_can_add_product_to_basket(browser, link):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}'
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
